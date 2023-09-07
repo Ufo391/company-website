8988742dfb46836c4ee3b8a8c2fc79f1cc944dc2
Ufo391 <nk-west@hotmail.de>
2023-09-07 11:46:19 +0200

    Add more configurations and packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [	AppComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
