b855d0212e65881d2c0caec499c0af12af0a5254
Ufo391 <nk-west@hotmail.de>
2023-12-20 13:19:22 +0100

    Add footer hyperlinks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MenubarModule } from 'primeng/menubar';
 import { ContentModule } from './content/content.module';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
@@ -22,6 +23,7 @@ import { HeaderComponent } from './header/header.component';
     CardModule,
     MenubarModule,
     ContentModule,
+    DividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
