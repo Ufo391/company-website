ed54fb62686295921af84882e6e6bbafa8761b61
Ufo391 <nk-west@hotmail.de>
2024-01-23 17:09:05 +0100

    Opacity animation for the header chapter text.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,54 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ChapterService } from '../services/chapter.service';
+
+const opacityHeaderInMs: number = 1500;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+
+  animations: [
+    trigger('opacityAnimation', [
+      state(
+        'on',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'off',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('off => on', [animate(`${opacityHeaderInMs}ms`)]),
+    ]),
+  ],
 })
 export class HeaderComponent implements OnInit {
   navbarfixed: boolean = false;
   sidebarVisible: boolean = false;
   isLanguageGerman: boolean = true;
-  currentYear: string = "";
+  currentYear: string = '';
+  animationStatus = 'off';
 
-  constructor(public chapterService: ChapterService) {}
+  constructor(public chapterService: ChapterService) {
+    this.chapterService.currentChapter$.subscribe((c) => {
+      this.animationStatus = 'off';
+      const id = setTimeout(() => {
+        this.animationStatus = 'on';
+      }, 100);
+
+    });
+  }
 
   ngOnInit() {
     let d: Date = new Date(Date.now());
