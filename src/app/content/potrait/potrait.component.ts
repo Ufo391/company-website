955d967d8fadce08bafdc557b735b790d7678618
Ufo391 <nk-west@hotmail.de>
2024-01-29 16:19:55 +0100

    Add fold out function to potrait component.

--- src/app/content/potrait/potrait.component.ts
+++ src/app/content/potrait/potrait.component.ts
@@ -12,12 +12,12 @@ import { LanguageService } from 'src/app/services/language.service';
 @Component({
   selector: 'app-potrait',
   templateUrl: './potrait.component.html',
-  styleUrls: ['./potrait.component.css'],
+  styleUrls: ['./potrait.component.scss'],
 })
 @UntilDestroy()
 export class PotraitComponent implements OnInit, AfterViewInit {
-  description: string =
-    'Ich bin ein erfahrener Softwareentwickler mit über 5 Jahren Berufserfahrung, einschließlich Consulting und fachlicher Führung. Meine Expertise liegt in .NET C# Entwicklung, Angular-Entwicklung und Microsoft Power Platform Entwicklung & Beratung. Ich beherrsche vielfältige Programmiersprachen und Technologien und bin versiert in agilen Methoden sowie Test-Driven Development. Ich habe erfolgreich kaufmännische Prozesse digitalisiert und zeichne mich durch die Fähigkeit aus, mich in unstrukturierte Umgebungen einzuarbeiten und durch methodische Herangehensweisen positive Ergebnisse zu liefern. Klare und offene Kommunikation mit Kunden und Teammitgliedern sind mir wichtig, um absolute Kundenzufriedenheit zu gewährleisten. Ich setze kontinuierliche Weiterbildung in neuen Technologien und Branchen ein, um dieses Ziel zu erreichen.';
+  isFoldOut: boolean = false;
+  foldOutIcon: string = 'pi pi-arrow-down';
 
   constructor(
     private chapterService: ChapterService,
@@ -29,9 +29,18 @@ export class PotraitComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    this.chapterService.addChapter(this.myElement, () => {}, () => {});
+    this.chapterService.addChapter(
+      this.myElement,
+      () => {},
+      () => {}
+    );
     this.lService.MasterData$.pipe(untilDestroyed(this)).subscribe((c) => {
       this.chapterService.translateChapter(this.myElement, c.aboutMe.title);
     });
+  }
+
+  toggleFoldOut(): void {
+    this.isFoldOut = !this.isFoldOut;
+    this.foldOutIcon = this.isFoldOut ? 'pi pi-arrow-up' : 'pi pi-arrow-down';
   }
 }
