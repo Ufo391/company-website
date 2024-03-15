27ba92ef208202165d6ef55a69a146139eb68a9b
Ufo391 <nk-west@hotmail.de>
2024-03-15 17:01:00 +0100

    Refactore layout.

--- src/app/content/projects/projects.styles.ts
+++ src/app/content/projects/projects.styles.ts
@@ -1,6 +1,6 @@
 export class STYLES_PROJECTS{
   public static readonly CARD_BG_CL = "#f7f7f7";
   public static readonly SCROLLPANEL_H_XL = "50vh";
-  public static readonly SCROLLPANEL_H_nXL = "66vh";
+  public static readonly SCROLLPANEL_H_nXL = "40vh";
   public static readonly BUTTON_CL = "inherit";
 }
