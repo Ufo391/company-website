890e70f1f8dfb58bec6c1e484248aca70a9b3c09
Ufo391 <nk-west@hotmail.de>
2024-03-20 09:15:25 +0100

    Add touch function.

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -17,6 +17,10 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 @UntilDestroy()
 export class ContentComponent implements OnInit, AfterViewInit {
   @ViewChild('invisible') myElement!: ElementRef;
+
+  touchStartX: number = 0;
+  touchStartY: number = 0;
+
   constructor(
     private chapterService: ChapterService,
     public lService: LanguageService
@@ -35,5 +39,22 @@ export class ContentComponent implements OnInit, AfterViewInit {
     this.lService.MasterData$.pipe(untilDestroyed(this)).subscribe((c) => {
       this.chapterService.translateChapter(this.myElement, c.companyName);
     });
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.touches[0].clientX;
+    this.touchStartY = event.touches[0].clientY;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    const touchEndX = event.changedTouches[0].clientX;
+    const touchEndY = event.changedTouches[0].clientY;
+
+    const deltaX = touchEndX - this.touchStartX;
+    const deltaY = touchEndY - this.touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      this.chapterService.onTouchMove(deltaX > 0);
+    }
   }
 }
